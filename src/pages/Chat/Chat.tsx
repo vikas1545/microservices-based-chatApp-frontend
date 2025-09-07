3f5d375603fd9a89fe04dc5f3a1d662181443722
vikas1545 <vikaskumar.vk0612@gmail.com>
2025-09-07 18:28:17 +0530

    done

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useAppData } from '../../context/AppContext';
+import Loading from '../../components/Loading';
 
 export default function Chat() {
+  const { isAuth, loading } = useAppData();
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isAuth && !loading) {
+      navigate('/login');
+    }
+  }, [isAuth, loading])
+
+if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div>Chat</div>
   )
